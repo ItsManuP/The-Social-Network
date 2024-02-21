6da9b4f6cfb1a5173e8a095ab55a9bbf3d83ecca
IsManuP <manuel140298@live.it>
2024-02-21 17:49:09 +0100

    files removed

--- database/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://root:root@blog.2cmxui4.mongodb.net/')
-  .then(() => console.log('Connesso a MongoDB!'))
-  .catch(err => console.error('Errore di connessione a MongoDB:', err));
-
-module.exports = mongoose;
