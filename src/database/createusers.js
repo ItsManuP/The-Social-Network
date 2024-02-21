fabfb799822e12ed6c1519766c1cfab2db3cdc5e
IsManuP <manuel140298@live.it>
2024-02-21 17:48:59 +0100

    createusers enable to add user to mongodb database(working); in data id has been removed from user(useless field)

---
+++ src/database/createusers.js
@@ -0,0 +1,22 @@
+const { User } = require('./data');
+
+async function addUserToDatabase(username,password,email) {
+    try {
+        // Create a new user instance
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+
+        // Save the user to the database
+        await newUser.save();
+
+        console.log('Utente aggiunto al database!');
+    } catch (error) {
+        console.error('Errore aggiunta utente al database', error);
+    }
+}
+
+
+module.exports = { addUserToDatabase };
