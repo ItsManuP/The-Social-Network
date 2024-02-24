e4b7ee476f48801016d4ddc4a62a824d39764241
IsManuP <manuel140298@live.it>
2024-02-24 17:47:19 +0100

    checking if the pw inserted is the correct on

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,13 +1,20 @@
 import { User } from '../../database/data';
+var bcrypt = require('bcryptjs');
 
 export default async function handler(req, res) {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ username, password });
-
+  const user = await User.findOne({ username });
   if (user) {
-    res.status(200).json({ status: 'Loggato' });
-  } else {
-    res.status(401).json({ status: 'Credenziali non corrette' });
+    const salt = user.salt;
+    const isPasswordValid = bcrypt.compareSync(password, user.password); // Compara la password chiara con l'hash salvato
+
+    if (isPasswordValid) {
+      res.status(200).json({ status: 'Loggato' });
+    } else {
+      res.status(401).json({ status: 'Credenziali non corrette' });
+    }
   }
 }
+
+
