2b9f16427eb507fb9d6502d0099cbe154a8f3a7a
IsManuP <manuel140298@live.it>
2024-02-25 15:28:50 +0100

    redirect if logged

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,8 +1,10 @@
 import { User } from '../../database/data';
+import { useRouter } from 'next/router';
 var bcrypt = require('bcryptjs');
 
 export default async function handler(req, res) {
   const { username, password } = req.body;
+  
 
   const user = await User.findOne({ username });
   if (user) {
@@ -10,7 +12,9 @@ export default async function handler(req, res) {
     const isPasswordValid = bcrypt.compareSync(password, user.password); // Compara la password chiara con l'hash salvato
 
     if (isPasswordValid) {
-      res.status(200).json({ status: 'Loggato' });
+      res.status(200).json({ status: 'Login effettuato correttamente' });
+      router.push('/');
+
     } else {
       res.status(401).json({ status: 'Credenziali non corrette' });
     }
