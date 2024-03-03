13af3b04d32e41abe0f4704d89bafb7d5ee7ccc3
ItsManuP <manuel140298@live.it>
2024-03-03 20:34:50 +0100

    components: footer&header

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer className="flex items-center h-16 border-t">
+        <div className="container flex items-center justify-end gap-4 px-4 text-sm md:gap-6">
+          <div className="flex items-center gap-2">
+          </div>
+          <div className="flex items-center gap-2">
+          </div>
+          <div className="ml-auto text-gray-500 dark:text-gray-400">© 2024 </div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
